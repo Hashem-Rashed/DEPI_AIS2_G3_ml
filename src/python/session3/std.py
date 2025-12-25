ccf7d4383415d17a4ccba44c27438286651d1935
Hashem-Rashed <hashemrashed248@gmail.com>
2025-12-25 23:53:55 +0200

    h

---
+++ src/python/session3/std.py
@@ -0,0 +1,14 @@
+def std_info(name="hashem",age=20):
+    """ 
+    this function take student information like name and age
+    param_1 :take user mane form user
+    type param_1: str
+    param_2 :take age form user
+    type param_2:int 
+
+    return :print student information for the name and age
+    type return: str,int
+
+    """
+   
+    return f"my name is {name} and my age is {age}"
