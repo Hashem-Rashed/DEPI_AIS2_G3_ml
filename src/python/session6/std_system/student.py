6db76eabd6d2bcd0c2020d35f5fe28d5ea461993
Hashem-Rashed <hashemrashed248@gmail.com>
2026-01-01 22:14:18 +0200

    h

---
+++ src/python/session6/std_system/student.py
@@ -0,0 +1,24 @@
+class Student:
+    _id_counter =1
+
+    def __init__(self,name):
+        self.student_id =Student._id_counter
+        Student._id_counter +=1
+        self.name = name
+        self.grades ={}
+        self.enrolled_courses =[]
+    def __str__(self):
+        return f"student ID : {self.student_id}, Nsme : {self.name} , Grades : {self.grades}"
+    
+    def __repr__ (self):
+        return f"student ID : {self.student_id}, Nsme : {self.name} , Grades : {self.grades}"
+    
+    def add_grade(self,course_id,grade):
+        if not 0<= grade <=100 :
+            raise ValueError("grades must be between 0 and 100")
+        self.grades[course_id] =grade 
+    def enrool_in_course(self,course):
+        if course in self.enrolled_courses:
+            print(f"{self.name} is already enrolled in {course}")
+        self.enrolled_courses.append(course)
+
---
+++ src/python/std_system/main.py
@@ -0,0 +1,15 @@
+from systemmanger import SystemManager
+
+def show_menu():
+    print("\n" + "=" * 50)
+    print("1. Add student")
+    print("2. Remove student")
+    print("3. Add course")
+    print("4. Remove course")
+    print("5. Search courses")
+    print("6. Record grade")
+    print("7. Get all students")
+    print("8. Get all courses")
+    print("9. Enroll course")
+    print("10. Exit")
+    print("=" * 50)
